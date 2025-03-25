2da712ee50336c9681004deaa57669875c4bc2af
Rishu00001 <ritiksingh7369@gmail.com>
2025-03-26 00:37:42 +0530

    set up vite,create routes and create responsive navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TiHome } from "react-icons/ti";
+import { IoSearch } from "react-icons/io5";
+import { RiPlayListFill } from "react-icons/ri";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <div className="w-full h-[100px] bg-black fixed bottom-0 md:top-0 text-white flex flex-row justify-around md:justify-center items-center gap-[50px] p-[20px]">
+      <Link to={"/"}>
+        <TiHome className="w-[25px] h-[25px]" />
+      </Link>
+      <Link to={"/search"}>
+        <IoSearch className="w-[25px] h-[25px]" />
+      </Link>
+      <Link to={"/playlist"}>
+        <RiPlayListFill className="w-[25px] h-[25px]" />
+      </Link>
+      <Link to={"/liked"}>
+        <FaHeart className="w-[25px] h-[25px]" />
+      </Link>
+    </div>
+  );
+}
+
+export default Nav;
